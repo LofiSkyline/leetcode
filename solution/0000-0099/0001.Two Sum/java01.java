6c9ac289859b1e96825e284d3ceae5d8baff8c0f
LofiSkyline <islandlmz@gmail.com>
2025-05-13 20:53:37 +0800

    01java

---
+++ solution/0000-0099/0001.Two Sum/java01.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class java01 {
+    public int[] Sum(int[] nums, int target){
+        Map<Integer, Integer> d = new HashMap<>();
+                for (int i = 0;; ++i) {
+            int x = nums[i];
+            int y = target - x;
+            if (d.containsKey(y)) {
+                return new int[] {d.get(y), i};
+            }
+            d.put(x, i);
+        }
+    }
+    
+
+    public static void main(String[] args){
+        java01 s  = new java01();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = s.Sum(nums, target);
+        System.out.println("Result: "+ Arrays.toString(result));
+    }
+    
+}
